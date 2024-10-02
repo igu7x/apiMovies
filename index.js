419bdbfef51ef1f0dcf63f2bddabb9f17f4b4367
Igu <igucupertino@gmail.com>
2024-10-02 09:35:47 -0300

    api

---
+++ index.js
@@ -0,0 +1,12 @@
+const { json } = require("body-parser");
+const express = require("express");
+const server = express();
+const filmes = require('./src/data/filmes.json');
+
+server.get("/filmes", (req, res) => {
+  return res.json(filmes);
+});
+
+server.listen(3000, () => {
+  console.log("Servidor está funcionando!");
+});
